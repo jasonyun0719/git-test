7203128b718bcd58601f5d02c9a72fed75b22b78
贠强 <1457628300@qq.com>
2020-05-19 09:51:24 +0800

    改变文件结构

--- src/com/briup/git/FirstClass.java
+++ src/com/briup/git/FirstClass.java
@@ -1,19 +1,17 @@
 package com.briup.git;
 
+import java.util.Scanner;
+
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
+        System.out.println("请输入名字。。。。");
+        Scanner sc = new Scanner(System.in);
+            String name =sc.nextLine();
 
+        FirstClass f = new FirstClass();
+        f.test(name);
     }
-    public void test(){
-        int i = 0;
-        i += 10;
+    public void test( String name){
+        System.out.println(name+"帅哥！");
     }
 }
